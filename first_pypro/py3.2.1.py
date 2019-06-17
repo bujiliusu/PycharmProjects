6d8abdce42cf8878fc2af600f935399fdd9c2094
bujiliusu <bujiliusu@163.com>
2019-06-17 22:17:35 +0800

    add py3.2.3

--- first_pypro/py3.2.1.py
+++ first_pypro/py3.2.1.py
@@ -51,7 +51,27 @@
 #     if hasattr(e, 'code'):
 #         print 'Error code:', e.code
 
+# import urllib2
+# response = urllib2.urlopen('http://www.zhihu.com')
+# isRedirected = response.geturl() == 'http://www.zhihu.com'
+
+# import urllib2
+# class RedirectHandler(urllib2.HTTPRedirectHandler):
+#     def http_error_301(self, req, fp, code, msg, headers):
+#         pass
+#     def http_error_302(self, req, fp, code, msg, headers):
+#         result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
+#         result.status = code
+#         result.newurl = result.geturl()
+#         return result
+# opener = urllib2.build_opener(RedirectHandler)
+# opener.open('http://www.zhihu.cn')
+
 import urllib2
-response = urllib2.urlopen('http://www.zhihu.com')
-isRedirected = response.geturl() == 'http://www.zhihu.com'
-print isRedirected
+proxy = urllib2.ProxyHandler({'http': '127.0.0.1:8087'})
+opener = urllib2.build_opener([proxy,])
+urllib2.install_opener(opener)
+response = urllib2.urlopen("http://www.zhihu.com")
+print response.read()
+
+
